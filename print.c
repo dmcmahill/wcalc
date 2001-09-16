91bf8fc072eb9db0acd5ddacf518418f9681dd5a
Dan McMahill <mcmahill@alum.mit.edu>
2001-09-16 15:56:15 -0400

    more work towards printing.

--- print.c
+++ print.c
@@ -1,4 +1,4 @@
-/* $Id$ */
+/* $Id: print.c,v 1.1 2001/09/15 23:56:38 dan Exp $ */
 
 /*
  * Copyright (c) 2001 Dan McMahill
@@ -33,18 +33,84 @@
  * SUCH DAMAGE.
  */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+
 #include <gtk/gtk.h>
 
 #include "print.h"
 
 
 static void print_browse_file(void);
-static void browse_ok_sel (GtkWidget *w, GtkFileSelection *fs);
+static void browse_ok_sel (GtkWidget *w, GtkWidget *fs);
 static void browse_cancel_sel (GtkWidget *w, GtkWidget *window);
 
+static int print_ps_header(FILE *fp);
+static void do_print(void);
+
+
+/* the print command */
+static GtkWidget *text_cmd;
+
+/* the print to file filename */
+static GtkWidget *text_fname;
+
+/* the "browse" button */
+static GtkWidget *browse_button;
+
+/* where to print */
+#define PRINT_TO_PRINTER  0
+#define PRINT_TO_FILE     1
+static int where_print;
+
+static void to_printer_pressed (GtkWidget *w, gpointer data)
+{
+  gtk_widget_set_sensitive (text_cmd, TRUE);
+  gtk_widget_set_sensitive (text_fname, FALSE);
+  where_print = PRINT_TO_PRINTER;
+  gtk_widget_set_sensitive (browse_button, FALSE);
+}
+
+static void to_file_pressed (GtkWidget *w, gpointer data)
+{
+  gtk_widget_set_sensitive (text_cmd, FALSE);
+  gtk_widget_set_sensitive (text_fname, TRUE);
+  where_print = PRINT_TO_FILE;
+  gtk_widget_set_sensitive (browse_button, TRUE);
+}
+
 static void ok_pressed (GtkWidget *w, GtkWidget *window)
+{
+  char *str;
+
+  str = gtk_entry_get_text( GTK_ENTRY(text_cmd) ); 
+  printf("ok_pressed():  print command = \"%s\"\n",str);
+
+  str = gtk_entry_get_text( GTK_ENTRY(text_fname) ); 
+  printf("ok_pressed():  print file = \"%s\"\n",str);
+
+  if (where_print == PRINT_TO_PRINTER)
+    printf("ok_pressed():  print to printer\n");
+  else if(where_print == PRINT_TO_FILE)
+    printf("ok_pressed():  print to file\n");
+  else
+    printf("ok_pressed():  print to UNKNOWN\n");
+
+  
+  do_print();
+
+  /* unmake it modal */
+  gtk_grab_remove(window);
+
+  /* blow away the window */
+  gtk_widget_destroy(window);
+
+}
+
+static void cancel_pressed (GtkWidget *w, GtkWidget *window)
 {
   /* unmake it modal */
   gtk_grab_remove(window);
@@ -60,13 +126,20 @@ void print_popup(void)
   GtkWidget *button;
   GtkWidget *label;
   GtkWidget *window;
-  GSList *group;
 
   GtkWidget *table;
-  GtkWidget *text_cmd;
-  GtkWidget *text_fname;
 
   GtkWidget *my_hbox;
+
+  /* the "File/Printer" radio button group */
+  GSList *where_group;
+
+  static int first_time=1;
+  
+  if (first_time){
+    where_print=PRINT_TO_PRINTER;
+    first_time = 0;
+  }
 
   /* create the "print" dialog */
   window = gtk_dialog_new();
@@ -102,7 +175,7 @@ void print_popup(void)
   /* Add the "Cancel" button and set its action */
   button = gtk_button_new_with_label ("Cancel");
   gtk_signal_connect(GTK_OBJECT(button), "clicked",
-		     GTK_SIGNAL_FUNC(ok_pressed),
+		     GTK_SIGNAL_FUNC(cancel_pressed),
 		     GTK_OBJECT(window));
   gtk_box_pack_end (GTK_BOX (my_hbox), button, FALSE, FALSE, 0);
   gtk_widget_show (button);
@@ -134,13 +207,22 @@ void print_popup(void)
 
 
   button = gtk_radio_button_new_with_label (NULL, "Printer");
+  gtk_signal_connect(GTK_OBJECT(button), "clicked",
+		     GTK_SIGNAL_FUNC(to_printer_pressed),
+		     NULL);
   gtk_table_attach_defaults (GTK_TABLE(table), button, 1, 2, 0, 1);
-  gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (button), TRUE);
+  if (where_print == PRINT_TO_PRINTER)
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (button), TRUE);
   gtk_widget_show (button);
 
-  group = gtk_radio_button_group (GTK_RADIO_BUTTON (button));
-  button = gtk_radio_button_new_with_label(group, "File");
+  where_group = gtk_radio_button_group (GTK_RADIO_BUTTON (button));
+  button = gtk_radio_button_new_with_label(where_group, "File");
+  gtk_signal_connect(GTK_OBJECT(button), "clicked",
+		     GTK_SIGNAL_FUNC(to_file_pressed),
+		     NULL);
   gtk_table_attach_defaults (GTK_TABLE(table), button, 2, 3, 0, 1);
+  if (where_print == PRINT_TO_FILE)
+    gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (button), TRUE);
   gtk_widget_show (button);
 
 
@@ -162,6 +244,10 @@ void print_popup(void)
   text_cmd = gtk_entry_new_with_max_length( 80 );
   gtk_entry_set_text(GTK_ENTRY(text_cmd),"lpr -Ppastry ");
   gtk_table_attach_defaults (GTK_TABLE(table), text_cmd, 1, 5, 1, 2);
+  if (where_print == PRINT_TO_PRINTER)
+    gtk_widget_set_sensitive (text_cmd, TRUE);
+  else
+    gtk_widget_set_sensitive (text_cmd, FALSE);
   gtk_widget_show(text_cmd);
 
 
@@ -183,18 +269,25 @@ void print_popup(void)
 
   text_fname = gtk_entry_new_with_max_length( 80 );
   gtk_entry_set_text(GTK_ENTRY(text_fname),"wcalc.ps");
-  gtk_entry_set_editable (GTK_ENTRY(text_fname), FALSE);
+  if (where_print == PRINT_TO_FILE)
+    gtk_widget_set_sensitive (text_fname, TRUE);
+  else
+    gtk_widget_set_sensitive (text_fname, FALSE);
   gtk_table_attach_defaults (GTK_TABLE(table), text_fname, 1, 4, 2, 3);
   gtk_widget_show(text_fname);
 
 
-  button = gtk_button_new_with_label ("Browse...");
+  browse_button = gtk_button_new_with_label ("Browse...");
   gtk_signal_connect(GTK_OBJECT(button), "clicked",
 		     GTK_SIGNAL_FUNC(print_browse_file), 
 		     GTK_OBJECT(window));
   //gtk_table_attach_defaults (GTK_TABLE(table), button, 4, 5, 2, 3);
-  gtk_table_attach (GTK_TABLE(table), button, 4, 5, 2, 3, 0,0,0,0);
-  gtk_widget_show (button);
+  gtk_table_attach (GTK_TABLE(table), browse_button, 4, 5, 2, 3, 0,0,0,0);
+  if (where_print == PRINT_TO_FILE)
+    gtk_widget_set_sensitive (browse_button, TRUE);
+  else
+    gtk_widget_set_sensitive (browse_button, FALSE);
+  gtk_widget_show (browse_button);
 
   /*
    * show the action area table
@@ -226,7 +319,8 @@ static void print_browse_file(void)
 
   /* Connect the ok_button to file_ok_sel function */
   gtk_signal_connect (GTK_OBJECT (GTK_FILE_SELECTION (filew)->ok_button),
-		      "clicked", (GtkSignalFunc) browse_ok_sel, filew );
+		      "clicked", (GtkSignalFunc) browse_ok_sel,
+		      GTK_OBJECT (filew) );
     
   /* Connect the cancel_button to destroy the widget */
   gtk_signal_connect(GTK_OBJECT (GTK_FILE_SELECTION (filew)->cancel_button),
@@ -244,11 +338,22 @@ static void print_browse_file(void)
 }
 
 /* Get the selected filename and print it to the console */
-static void browse_ok_sel (GtkWidget *w, GtkFileSelection *fs)
+static void browse_ok_sel (GtkWidget *w, GtkWidget *fs)
 {
     g_print ("%s\n", 
-	     gtk_file_selection_get_filename (GTK_FILE_SELECTION (fs)));
+	     gtk_file_selection_get_filename (GTK_FILE_SELECTION
+					      (fs)));
 
+  gtk_entry_set_text(GTK_ENTRY(text_fname),
+	     gtk_file_selection_get_filename (GTK_FILE_SELECTION
+					      (fs)));
+
+  /* unmake it modal */
+  gtk_grab_remove(fs);
+  
+  /* blow away the window */
+  gtk_widget_destroy(fs);
+  
 }
 
 static void browse_cancel_sel (GtkWidget *w, GtkWidget *window)
@@ -261,3 +366,72 @@ static void browse_cancel_sel (GtkWidget *w, GtkWidget *window)
 }
 
 
+static void do_print()
+{
+  FILE *fp;
+  char *str;
+
+  switch(where_print){
+  case PRINT_TO_FILE:
+    str = gtk_entry_get_text( GTK_ENTRY(text_fname) ); 
+    if ( (fp = fopen(str,"w")) == NULL){
+      fprintf(stderr,"do_print():  fopen of \"%s\"failed\n\n",str);
+      return;
+    }
+    print_ps_header(fp);
+    fclose(fp);
+    break;
+
+  case PRINT_TO_PRINTER:
+    str = gtk_entry_get_text( GTK_ENTRY(text_cmd) ); 
+    if ( (fp = popen(str,"w")) == NULL){
+      fprintf(stderr,"do_print():  popen of \"%s\"failed\n\n",str);
+      return;
+    }
+    print_ps_header(fp);
+    pclose(fp);
+    break;
+
+    
+  default:
+    fprintf(stderr,"do_print():  unknown destination (%d)\n",where_print);
+    return;
+    break;
+  }
+  
+
+}
+
+
+static int print_ps_header(FILE *fp)
+{
+  double fontsize=12;
+  double leftmargin=1.0;
+  double topmargin=1.5;
+  double bottommargin=1.5;
+  double paperheight=11;
+  double paperwidth=8.5;
+  double tab1=0.75;
+  double tab2=0.2;
+  double tab3=0.6;
+  double tab4=0.5;
+
+  time_t now;
+
+  int bbox_llx, bbox_lly, bbox_urx, bbox_ury;
+
+  tab2 += tab1;
+  tab3 += tab2;
+  tab4 += tab3;
+  
+  now = time(NULL);
+
+  bbox_llx=101;
+  bbox_lly=664;
+  bbox_urx=309;
+  bbox_ury=775;
+
+#include "ps_header.c"
+
+  return 0;
+}
