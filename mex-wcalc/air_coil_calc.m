6a5a07102fd815a20683761b07280787d4019705
Dan McMahill <mcmahill@alum.mit.edu>
2001-10-18 21:42:48 -0400

    preliminary pass at some matlab support

---
+++ mex-wcalc/air_coil_calc.m
@@ -0,0 +1,52 @@
+function [L,Q,SRF]=air_coil_calc(N,len,AWG,dia,freq)
+%AIR_COIL_CALC    Calculate inductance parameters for single-layer solenoids
+%
+%  [L,Q,SRF]=air_coil_calc(N,len,AWG,dia,freq)
+%  calculates:
+%    L    = Inductance (Henries)
+%    Q    = Quality Factor at f = freq
+%    SRF  = Self Resonant Frequency (Hz)
+%  from:
+%    N    = Number of Turns
+%    len  = length of inductor (m)
+%    AWG  = Wire size (A.W.G.)
+%    dia  = Inside Diameter of Inductor (m)
+%    freq = Frequency for Q Analysis (Hz)
+%
+%  Part of Wcalc
+%  See Also:  AIR_COIL_SYN
+
+% $Id$
+%
+% Copyright (c) 2001 Dan McMahill
+% All rights reserved.
+%
+% This code is derived from software written by Dan McMahill
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions
+% are met:
+% 1. Redistributions of source code must retain the above copyright
+%    notice, this list of conditions and the following disclaimer.
+% 2. Redistributions in binary form must reproduce the above copyright
+%    notice, this list of conditions and the following disclaimer in the
+%    documentation and/or other materials provided with the distribution.
+% 3. All advertising materials mentioning features or use of this software
+%    must display the following acknowledgement:
+%        This product includes software developed by Dan McMahill
+%  4. The name of the author may not be used to endorse or promote products
+%     derived from this software without specific prior written permission.
+% 
+%  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+%  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+%  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+%  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+%  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+%  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+%  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+%  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+%  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+%  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+%  SUCH DAMAGE.
+
+
