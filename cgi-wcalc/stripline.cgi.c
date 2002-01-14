c2523554d1fe924e91ff6688729b526f39d67ee9
Dan McMahill <mcmahill@alum.mit.edu>
2002-01-13 21:55:02 -0500

    add all the code and html stuff to use user units everywhere except in the incremental circuit model output (still need that).
    don't display the incremental circuit model as it takes up too much space.
    should add a link for that.

--- cgi-wcalc/stripline.cgi.c
+++ cgi-wcalc/stripline.cgi.c
@@ -1,7 +1,7 @@
-/* $Id: stripline.cgi.c,v 1.1 2001/10/05 01:28:31 dan Exp $ */
+/* $Id: stripline.cgi.c,v 1.2 2001/11/12 11:55:49 dan Exp $ */
 
 /*
- * Copyright (c) 2001 Dan McMahill
+ * Copyright (c) 2001, 2002 Dan McMahill
  * All rights reserved.
  *
  * This code is derived from software written by Dan McMahill
@@ -37,11 +37,22 @@
  * a cgi interface to the stripline calculator
  */
 
-//#define DEBUG
+/* #define DEBUG */
+
+#include "config.h"
 
 #include <stdio.h>
+
+/* CGI specific */
 #include "cgic.h"
+#include "cgi-units.h"
+
+/* libwcalc */
 #include "stripline.h"
+#include "misc.h"
+#include "physconst.h"
+
+/* ID's for this module */
 #include "stripline_id.h"
 
 #define ACTION_LEN  20
@@ -86,97 +97,28 @@ int cgiMain(void){
 
   /* stripline variables */
   stripline_line *line;
-  double freq, freq_Hz;
+  double freq;
 
   double rho,rough,tmet,w,l,h,es,tand;
   double Ro=0.0;
-  double Xo=0.0;
   double elen;
+  int i;
 
-  /* delay on the line (ns) */
-  double delay;
 
-  /* The circuit model */
-  double Ls, Rs, Cs, Gs;
-
-/* Put out the CGI header */
+  /* 
+   * uncomment to be able to run in the debugger. 
+   * access the CGI URL that gives the problem, then change foo.cgi to
+   * capture.cgi and reload.  That dumps the env to /tmp/capcgi.dat.
+   */
+  /* cgiReadEnvironment("/tmp/capcgi.dat"); */
+  
+  /* Put out the CGI header */
   cgiHeaderContentType("text/html");  
 
   /* create the stripline line */
+  fprintf(cgiOut,"<pre>");
   line = stripline_line_new();
-
-  /*
-   * extract the parameters from the CGI form and use them to populate
-   * the stripline structure
-   */
-
-
-  /* Metal resistivity relative to copper */
-  if(cgiFormDoubleBounded("rho",&rho,0.0001,1000.0,defRHO) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  /* Metal thickness (m) */
-  if(cgiFormDoubleBounded("tmet",&tmet,0.0001,1000.0,defTMET) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  /* Metalization roughness */
-  if(cgiFormDoubleBounded("rough",&rough,0.0001,1000.0,defRGH) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  /* Stripline width */
-  if(cgiFormDoubleBounded("w",&w,0.0001,1000.0,defW) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  /* Stripline length */
-  if(cgiFormDoubleBounded("l",&l,1.0,100000.0,defL) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  /* Substrate dielectric thickness */
-  if(cgiFormDoubleBounded("h",&h,0.0001,1000.0,defH) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  /* Substrate relative permittivity */
-  if(cgiFormDoubleBounded("es",&es,0.0001,1000.0,defES) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  /* Substrate loss tangent */
-  if(cgiFormDoubleBounded("tand",&tand,0.0001,1000.0,defTAND) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  /* Frequency of operation (MHz) */
-  if(cgiFormDoubleBounded("freq",&freq,1e-6,1e6,defFREQ) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-
-  /* electrical parameters: */
-  if(cgiFormDoubleBounded("Ro",&Ro,0.0001,1000.0,defRO) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
-  if(cgiFormDoubleBounded("elen",&elen,0.0001,1000.0,defELEN) !=
-     cgiFormSuccess){
-    input_err=1;
-  }
-
+  fprintf(cgiOut,"</pre>");
 
   /* flags to the program: */
   if(cgiFormStringNoNewlines("analyze",str_action,ACTION_LEN) ==
@@ -206,34 +148,133 @@ int cgiMain(void){
   else{
     action = LOAD;
   }
-  
 
-  if ( (action == RESET) || (action == LOAD) ){
-    w     = defW;
-    l     = defL;
-    
-    tmet  = defTMET;
-    rho   = defRHO;
-    rough = defRGH;
+  /*
+   * extract the parameters from the CGI form and use them to populate
+   * the stripline structure
+   */
 
-    h     = defH;
-    es    = defES;
-    tand  = defTAND;
+  if ( (action != RESET) && (action != LOAD) ) {
+    /* Metal resistivity relative to copper */
+    if(cgiFormDoubleBounded("rho",&rho,0.0,1000.0,defRHO) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
 
-    Ro    = defRO;
-    elen  = defELEN;
-  }
+    /* Metal thickness (m) */
+    if(cgiFormDoubleBounded("tmet",&tmet,0.0,1000.0,defTMET) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+    /* units */
+    if (cgiFormRadio("tmet_units",units_strings_get(length_units),units_size(length_units),&i,0) !=
+	cgiFormSuccess){
+      input_err=1;
+    }
+    line->subs->tmet_units = length_units[i].name;
+    line->subs->tmet_sf = length_units[i].sf;
 
+    /* Metalization roughness */
+    if(cgiFormDoubleBounded("rough",&rough,0.0,1000.0,defRGH) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+    /* units */
+    if (cgiFormRadio("rough_units",units_strings_get(length_units),units_size(length_units),&i,0) !=
+	cgiFormSuccess){
+      input_err=1;
+    }
+    line->subs->rough_units = length_units[i].name;
+    line->subs->rough_sf = length_units[i].sf;
+
+
+    /* Stripline width */
+    if(cgiFormDoubleBounded("w",&w,0.0001,1000.0,defW) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+    /* units */
+    if (cgiFormRadio("w_units",units_strings_get(length_units),units_size(length_units),&i,0) !=
+	cgiFormSuccess){
+      input_err=1;
+    }
+    line->w_units = length_units[i].name;
+    line->w_sf = length_units[i].sf;
+
+    /* Stripline length */
+    if(cgiFormDoubleBounded("l",&l,1.0,100000.0,defL) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+    /* units */
+    if (cgiFormRadio("l_units",units_strings_get(length_units),units_size(length_units),&i,0) !=
+	cgiFormSuccess){
+      input_err=1;
+    }
+    line->l_units = length_units[i].name;
+    line->l_sf = length_units[i].sf;
+
+
+    /* Substrate dielectric thickness */
+    if(cgiFormDoubleBounded("h",&h,0.0001,1000.0,defH) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+    /* units */
+    if (cgiFormRadio("h_units",units_strings_get(length_units),units_size(length_units),&i,0) !=
+	cgiFormSuccess){
+      input_err=1;
+    }
+    line->subs->h_units = length_units[i].name;
+    line->subs->h_sf = length_units[i].sf;
+
+
+    /* Substrate relative permittivity */
+    if(cgiFormDoubleBounded("es",&es,0.0001,1000.0,defES) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+
+    /* Substrate loss tangent */
+    if(cgiFormDoubleBounded("tand",&tand,0.0,1000.0,defTAND) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+
+    /* Frequency of operation (MHz) */
+    if(cgiFormDoubleBounded("freq",&freq,1e-6,1e6,defFREQ) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+    /* Frequency of operation units */
+    if (cgiFormRadio("freq_units",units_strings_get(frequency_units),units_size(frequency_units),&i,0) !=
+	cgiFormSuccess){
+      input_err=1;
+    }
+    line->freq_units = frequency_units[i].name;
+    line->freq_sf = frequency_units[i].sf;
+
+
+    /* electrical parameters: */
+    if(cgiFormDoubleBounded("Ro",&Ro,0.0001,1000.0,defRO) !=
+       cgiFormSuccess){
+      input_err=1;
+    }
+
+    if(cgiFormDoubleBounded("elen",&elen,0.0001,1000.0,defELEN) !=
+       cgiFormSuccess){
+      input_err=1;
+    } 
 
   /* copy data over to the line structure */
-  line->w           = w     ;
-  line->l           = l     ;
-  line->subs->h     = h     ;
-  line->subs->tmet  = tmet  ;
-  line->subs->rough = rough ;
-  
+  line->w           = w*line->w_sf;
+  line->l           = l*line->l_sf;
+  line->subs->h     = h*line->subs->h_sf;
+  line->subs->tmet  = tmet*line->subs->tmet_sf;
+  line->subs->rough = rough*line->subs->rough_sf;
+
   /* convert to Hz from MHz */
-  freq_Hz = freq * 1.0e6;
+  line->freq = freq * line->freq_sf;
   
   /* copy over the other parameters */
   line->subs->tand  = tand;
@@ -245,85 +286,75 @@ int cgiMain(void){
   line->Xo = 0.0;
   line->len = elen;
 
+  } /* if ( (action != RESET) && (action != LOAD) ) */
+
   
-  switch (action){
-  case ANALYZE:
 #ifdef DEBUG
     fprintf(cgiOut,"<pre>\n");
     fprintf(cgiOut,"CGI: --------------- Stripline  Analysis -----------\n");
-    fprintf(cgiOut,"CGI: Metal width                 = %g mil\n",line->w);
-    fprintf(cgiOut,"CGI: Metal length                = %g mil\n",line->l);
-    fprintf(cgiOut,"CGI: Metal thickness             = %g mil\n",line->subs->tmet);
+    fprintf(cgiOut,"CGI: Action                      = %d\n",action);
+    fprintf(cgiOut,"CGI: -------------- ---------------------- ----------\n");
+    fprintf(cgiOut,"CGI: Metal width                 = %g %s\n",
+	    line->w/line->w_sf,line->w_units);
+    fprintf(cgiOut,"CGI: Metal length                = %g %s\n",
+	    line->l/line->l_sf,line->l_units);
+    fprintf(cgiOut,"CGI: Metal thickness             = %g %s\n",
+	    line->subs->tmet/line->subs->tmet_sf,line->subs->tmet_units);
     fprintf(cgiOut,"CGI: Metal resistivity rel to Cu = %g \n",line->subs->rho);
-    fprintf(cgiOut,"CGI: Metal surface roughness     = %g mil-rms\n",line->subs->rough);
-    fprintf(cgiOut,"CGI: Substrate thickness         = %g mil\n",line->subs->h);
+    fprintf(cgiOut,"CGI: Metal surface roughness     = %g %s-rms\n",
+	    line->subs->rough/line->subs->rough_sf,line->subs->rough_units);
+    fprintf(cgiOut,"CGI: Substrate thickness         = %g %s\n",
+	    line->subs->h/line->subs->h_sf, line->subs->h_units);
     fprintf(cgiOut,"CGI: Substrate dielectric const. = %g \n",line->subs->er);
     fprintf(cgiOut,"CGI: Substrate loss tangent      = %g \n",line->subs->tand);
-    fprintf(cgiOut,"CGI: Frequency                   = %g MHz\n",freq_Hz/1e6); 
+    fprintf(cgiOut,"CGI: Frequency                   = %g %s\n",
+	    line->freq/line->freq_sf, line->freq_units); 
     fprintf(cgiOut,"CGI: -------------- ---------------------- ----------\n");
     fprintf(cgiOut,"</pre>\n");
 #endif
+  
+  switch (action){
+  case ANALYZE:
 
     /* 
      * in case stripline_calc has some error output, surround it
      * with <pre></pre> so we can read it ok.
      */
     fprintf(cgiOut,"<pre>");
-    stripline_calc(line,freq_Hz);
+    stripline_calc(line,line->freq);
     fprintf(cgiOut,"</pre>\n");
 
     break;
 
   case SYNTH_H:
     fprintf(cgiOut,"<pre>");
-    stripline_syn(line,freq_Hz,SLISYN_H);
+    stripline_syn(line,line->freq,SLISYN_H);
     fprintf(cgiOut,"</pre>\n");
-    h = line->subs->h;
     break;
 
   case SYNTH_W:
     fprintf(cgiOut,"<pre>");
-    stripline_syn(line,freq_Hz,SLISYN_W);
+    stripline_syn(line,line->freq,SLISYN_W);
     fprintf(cgiOut,"</pre>\n");
-    w = line->w;
     break;
 
   case SYNTH_ES:
     fprintf(cgiOut,"<pre>");
-    stripline_syn(line,freq_Hz,SLISYN_ER);
+    stripline_syn(line,line->freq,SLISYN_ER);
     fprintf(cgiOut,"</pre>\n");
-    es = line->subs->er;
     break;
 
   case SYNTH_L:
     fprintf(cgiOut,"<pre>");
-    stripline_syn(line,freq_Hz,SLISYN_L);
+    stripline_syn(line,line->freq,SLISYN_L);
     fprintf(cgiOut,"</pre>\n");
-    l = line->l;
     break;
 
   }
 
-  /* extract the incremental circuit model */
-  Ls = line->Ls;
-  Rs = line->Rs;
-  Cs = line->Cs;
-  Gs = line->Gs;
-  Ro   = line->Ro;
-  Xo   = line->Xo;
 
-  /* electrical and physical length */
-  elen = line->len;
-  l = line->l;
-
-  /*
-   * delay on line (ns)
-   * 2 pi f Td = elen pi/180
-   * Td = (pi/180) elen/(2 pi f) = elen/(360 f)
-   * and in ns,
-   * Td = elen/(360 f *1e-9)
-   */
-  delay = elen /(360.0 * freq_Hz * 1e-9);
+  /* autoscale the delay output */
+  units_autoscale(time_units,&line->delay_sf,&line->delay_units,line->delay);
 
   /* include the HTML output */
 #include "stripline_html.c"
