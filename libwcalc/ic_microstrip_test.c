3c78d9e01ce0b67cbb9c8bd9867ca06b4c9bef43
Dan McMahill <mcmahill@alum.mit.edu>
2002-05-08 06:38:33 -0400

    add more tests

--- libwcalc/ic_microstrip_test.c
+++ libwcalc/ic_microstrip_test.c
@@ -1,4 +1,4 @@
-/* $Id$ */
+/* $Id: ic_microstrip_test.c,v 1.1 2002/02/16 15:50:43 dan Exp $ */
 
 /*
  * Copyright (c) 2002 Dan McMahill
@@ -115,36 +115,72 @@ int main(int argc, char **argv)
 
   ic_microstrip_calc(line,freq);
 
-  printf("-------------- IC Microstrip Analysis ----------\n");
-  printf("Metal width                 = %g um\n",line->w*1e6);
-  printf("Oxide thickness             = %g um\n",line->subs->tox*1e6);
-  printf("Oxide dielectric const.     = %g \n",line->subs->eox);
-  printf("Substrate thickness         = %g um\n",line->subs->h*1e6);
-  printf("Substrate dielectric const. = %g \n",line->subs->es);
-  printf("Substrate conductivity      = %g 1/(ohm-cm)\n",line->subs->sigmas*0.01);
-  printf("Substrate resistivity       = %g ohm-cm\n",100.0/(line->subs->sigmas));
-  printf("Frequency                   = %g GHz\n",freq/1e9); 
-  printf("-------------- ---------------------- ----------\n");
-  printf("Zo (ohms)                   = %g + j%g\n",line->Ro,line->Xo);
-  printf("Slow wave factor            = %g\n",line->keff);
-  printf("Lmis                        = %g nH/mm\n",1e6*line->Lmis);
-  printf("Rmis                        = %g Ohm/mm\n",1e-3*line->Rmis);
-  printf("Cmis                        = %g pF/mm\n",1e9*line->Cmis);
-  printf("Gmis                        = %g S/mm\n",1e-3*line->Gmis);
-  printf("-------------- ---------------------- ----------\n");
-
-
-
-  /*
-  printf("------------------------------------\n");
-  w=0.050;
-  while(w<=0.120){
-    printf("Zustrip (w=%6.4f) = %6.4f\n",w,Zustrip(h,w,t,er));
-    w = w + 0.010;
+  if (argc > 1) {
+    printf("-------------- IC Microstrip Analysis ----------\n");
+    printf("Metal width                 = %g um\n",line->w*1e6);
+    printf("Oxide thickness             = %g um\n",line->subs->tox*1e6);
+    printf("Oxide dielectric const.     = %g \n",line->subs->eox);
+    printf("Substrate thickness         = %g um\n",line->subs->h*1e6);
+    printf("Substrate dielectric const. = %g \n",line->subs->es);
+    printf("Substrate conductivity      = %g 1/(ohm-cm)\n",line->subs->sigmas*0.01);
+    printf("Substrate resistivity       = %g ohm-cm\n",100.0/(line->subs->sigmas));
+    printf("Frequency                   = %g GHz\n",freq/1e9); 
+    printf("-------------- ---------------------- ----------\n");
+    printf("Zo (ohms)                   = %g + j%g\n",line->Ro,line->Xo);
+    printf("Slow wave factor            = %g\n",line->keff);
+    printf("Lmis                        = %g nH/mm\n",1e6*line->Lmis);
+    printf("Rmis                        = %g Ohm/mm\n",1e-3*line->Rmis);
+    printf("Cmis                        = %g pF/mm\n",1e9*line->Cmis);
+    printf("Gmis                        = %g S/mm\n",1e-3*line->Gmis);
+    printf("-------------- ---------------------- ----------\n");
   }
-  printf("------------------------------------\n");
-  */
 
+  else {
+  printf("------------------------------------\n");
+  w=50.0e-6;
+  while(w<=120.0e-6){
+    line->w = w;
+    ic_microstrip_calc(line,freq);
+    printf("w=%8.3f um, Zo=(%6.3f+j%6.3f) ohms, Lmis=%8.3f nH/mm,",
+	   line->w*1e6,line->Ro,line->Xo,line->Lmis*1e6);
+    printf("Rmis=%8.3f Ohm/mm, Cmis=%8.3f pF/mm, Gmis=%8.3f S/mm\n",
+	   line->Rmis*1e-3,line->Cmis*1e9,line->Gmis*1e-3);
+	   
+    w = w + 10.0e-6;
+  }
+
+
+  printf("------------------------------------\n");
+  w=50.0e-6;
+  line->w = w;
+  freq=100e6;
+  while(freq<=10e9){
+    line->freq = freq;
+    ic_microstrip_calc(line,freq);
+    printf("freq=%8.3f GHz, Zo=(%6.3f+j%6.3f) ohms, Lmis=%8.3f nH/mm,",
+	   freq*1e-9,line->Ro,line->Xo,line->Lmis*1e6);
+    printf("Rmis=%8.3f Ohm/mm, Cmis=%8.3f pF/mm, Gmis=%8.3f S/mm\n",
+	   line->Rmis*1e-3,line->Cmis*1e9,line->Gmis*1e-3);
+	   
+    freq=1.2*freq;
+  }
+
+  printf("------------------------------------\n");
+  freq=2.4e9;
+  line->freq = freq;
+  w=1;
+  line->subs->sigmas=w;
+  while(w<=100){
+    line->subs->sigmas = w;
+    ic_microstrip_calc(line,freq);
+    printf("sigma_s=%8.3f [ohm-cm]^-1, Zo=(%6.3f+j%6.3f) ohms, Lmis=%8.3f nH/mm,",
+	   w*0.01,line->Ro,line->Xo,line->Lmis*1e6);
+    printf("Rmis=%8.3f Ohm/mm, Cmis=%8.3f pF/mm, Gmis=%8.3f S/mm\n",
+	   line->Rmis*1e-3,line->Cmis*1e9,line->Gmis*1e-3);
+	   
+    w=1.2*w;
+  }
+  }
   return 0;
 }
 
