60ae6a0e583736fa6ec63256ee5ea92b724d0337
Dan McMahill <mcmahill@alum.mit.edu>
2002-08-06 21:22:34 -0400

    remove some unused code and add some comments

--- gtk-wcalc/coax_gui.c
+++ gtk-wcalc/coax_gui.c
@@ -1,4 +1,4 @@
-/* $Id: coax_gui.c,v 1.15 2002/07/05 03:22:08 dan Exp $ */
+/* $Id: coax_gui.c,v 1.16 2002/07/05 23:18:39 dan Exp $ */
 
 /*
  * Copyright (c) 1999, 2000, 2001, 2002 Dan McMahill
@@ -424,6 +424,8 @@ static void values_init(coax_gui *gui, GtkWidget *parent)
   gtk_misc_set_alignment(GTK_MISC(gui->units_t),0,0);
 
 
+  /* ---------------- Characteristic impedance -------------- */
+
   text = gtk_label_new( "Z0" );
   gtk_table_attach(GTK_TABLE(table), text, 5, 6, 0, 1, 0,0,XPAD,YPAD);
   gtk_widget_show(text);
@@ -433,6 +435,8 @@ static void values_init(coax_gui *gui, GtkWidget *parent)
 		   GTK_EXPAND|GTK_FILL,0,XPAD,YPAD);
   gtk_misc_set_alignment(GTK_MISC(text),0,0);
 
+  /* ---------------- Electrical length -------------- */
+
   text = gtk_label_new( "Elen" );
   gtk_table_attach(GTK_TABLE(table), text, 5, 6, 1, 2,
 		   GTK_EXPAND|GTK_FILL,0,XPAD,YPAD);
@@ -441,6 +445,8 @@ static void values_init(coax_gui *gui, GtkWidget *parent)
   gtk_table_attach(GTK_TABLE(table), text, 7, 8, 1, 2,
 		   GTK_EXPAND|GTK_FILL,0,XPAD,YPAD);
   gtk_misc_set_alignment(GTK_MISC(text),0,0);
+
+  /* ---------------- TE10 Cutoff -------------- */
 
   text = gtk_label_new( "TE10 Cutoff" );
   gtk_table_attach(GTK_TABLE(table), text, 5, 6, 2, 3, 0,0,XPAD,YPAD);
@@ -456,15 +462,6 @@ static void values_init(coax_gui *gui, GtkWidget *parent)
   text = gtk_label_new( "Frequency" );
   gtk_table_attach(GTK_TABLE(table), text,
 		   5, 6, 3, 4, 0,0,XPAD,YPAD);
-
-#ifdef notdef
-  hbox=gtk_hbox_new(FALSE,0);
-  gtk_table_attach(GTK_TABLE(table), hbox,
-		   7, 8, 3, 4, GTK_EXPAND|GTK_FILL,0,XPAD,YPAD);
-  gui->menu_freq_units = 
-    units_menu_new(frequency_units,0,(gpointer) gui,freq_units_changed);
-  gtk_box_pack_start (GTK_BOX (hbox),gui->menu_freq_units,FALSE,FALSE,0);
-#endif
 
   text = wc_composite_units_menu_new(gui->freq_units,WC_WCALC(gui),&ug,
 				     wc_composite_units_menu_changed);
@@ -488,6 +485,8 @@ static void values_init(coax_gui *gui, GtkWidget *parent)
 				     wc_composite_units_menu_changed);
   gtk_table_attach(GTK_TABLE(table), text, 7, 8, 4, 5, 
 		   GTK_EXPAND|GTK_FILL,0,XPAD,YPAD);
+
+  /* ---------------- RHO_b label/entry/units menu -------------- */
 
   text = gtk_label_new( "RHO_b" );
   gtk_table_attach(GTK_TABLE(table), text, 7, 8, 5, 6, 
