3ae25320a7d600c7c025cd986e5ba051df34513f
Dan McMahill <mcmahill@alum.mit.edu>
2004-08-09 08:56:22 -0400

    add a 'wrapper' header which includes all the libwcalc headers.  Point the config files to includedir so that we don't pollute header file namespace.

---
+++ libwcalc/libwcalc.h
@@ -0,0 +1,60 @@
+/* $Id: units.h,v 1.6 2004/07/26 22:22:30 dan Exp $ */
+
+/*
+ * Copyright (c) 2004 Dan McMahill
+ * All rights reserved.
+ *
+ * This code is derived from software written by Dan McMahill
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *        This product includes software developed by Dan McMahill
+ * 4. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+
+#ifndef __LIBWCALC_H__
+#define __LIBWCALC_H__
+
+#include <wcalc/air_coil.h>
+#include <wcalc/air_coil_loadsave.h>
+#include <wcalc/coax.h>
+#include <wcalc/coax_loadsave.h>
+#include <wcalc/coupled_microstrip.h>
+#include <wcalc/coupled_microstrip_loadsave.h>
+#include <wcalc/ic_microstrip.h>
+#include <wcalc/ic_microstrip_loadsave.h>
+#include <wcalc/mathutil.h>
+#include <wcalc/microstrip.h>
+#include <wcalc/microstrip_loadsave.h>
+#include <wcalc/misc.h>
+#include <wcalc/physconst.h>
+#include <wcalc/stripline.h>
+#include <wcalc/stripline_loadsave.h>
+#include <wcalc/units.h>
+#include <wcalc/wcalc_loadsave.h>
+
+#endif /*  __LIBWCALC_H__  */
+
+
