93fdfa62325bccc9ec83ecab9f65bb155c8ef103
Dan McMahill <mcmahill@alum.mit.edu>
2005-09-09 10:54:47 -0400

    make this more verbose and abort on errors

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 #
-# $Id: autogen.sh,v 1.1.1.1 2003/02/20 00:22:38 danmc Exp $
+# $Id: autogen.sh,v 1.3 2005/09/06 04:31:10 danmc Exp $
 #
 # Run the various GNU autotools to bootstrap the build
 # system.  Should only need to be done once.
@@ -9,8 +9,25 @@
 CONFIG_SHELL=/bin/sh
 export CONFIG_SHELL
 
-aclocal
-autoheader
-automake -a -c --foreign
-autoconf
-./configure $@
+echo "Running aclocal..."
+aclocal $ACLOCAL_FLAGS || exit 1
+echo "Done with aclocal"
+
+echo "Running autoheader..."
+autoheader || exit 1
+echo "Done with autoheader"
+
+echo "Running automake..."
+automake -a -c --foreign || exit 1
+echo "Done with automake"
+
+echo "Running autoconf..."
+autoconf || exit 1
+echo "Done with autoconf"
+
+echo "Running ./configure $@"
+./configure $@ || exit 1
+echo "Done with configure"
+
+echo "All done with autogen.sh"
+
