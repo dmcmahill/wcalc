ff587ee6d82dfa5658bdc4d36e3d839be9272f18
Dan McMahill <mcmahill@alum.mit.edu>
2005-10-19 20:37:09 -0400

    - move some common code out of the mex and octave directories into sciman.mk. - put automake conditional checks around the rebuilding of the documentation.   Normally, the preformatted docs will be distributed.

--- sci-wcalc/sciman.mk
+++ sci-wcalc/sciman.mk
@@ -1,5 +1,5 @@
 ## -*- Makefile -*-
-## $Id: sciman.mk,v 1.5 2004/11/04 03:22:17 dan Exp $
+## $Id: sciman.mk,v 1.6 2005/10/19 00:25:57 dan Exp $
 ##
 
 ## Copyright (c) 2001, 2004, 2005 Dan McMahill
@@ -34,7 +34,7 @@
 ##  SUCH DAMAGE.
 ##
 
-SUFFIXES+= .cat .man .htm .xml .txt
+SUFFIXES+= .cat .man .htm .xml .txt .m
 
 GROFF=		@GROFF@
 NEQN=		@NEQN@
@@ -43,6 +43,14 @@ W3M=		@W3M@
 XSLTPROC=	@XSLTPROC@
 
 .man.cat : 
+if MISSING_NROFF
+	@echo "****************************************************"
+	@echo "WARNING:  groff/tbl/neqn were not found on your system but"
+	@echo "          $@ is out of date and needs to be"
+	@echo "          rebuilt.  Changes to $<"
+	@echo "          will be ignored."
+	@echo "****************************************************"
+else
 	@echo "****************************************************"
 	@echo "Processing $*.man to produce $*.cat"
 	@echo "****************************************************"
@@ -55,13 +63,62 @@ XSLTPROC=	@XSLTPROC@
 			t=1; \
 		     } \
 		     {print;}' > $@
+endif
 
 .xml.man :
+if MISSING_XSLT
+	@echo "****************************************************"
+	@echo "WARNING:  xsltproc and/or w3m were not found on your"
+	@echo "          system but $@ is out of date and needs"
+	@echo "          to be rebuilt.  Changes to $<"
+	@echo "          will be ignored."
+	@echo "****************************************************"
+else
 	${XSLTPROC} --stringparam program "${TARGETPROGRAM}" $(top_srcdir)/sci-wcalc/manpage.xsl $< > $@
+endif
 
 .xml.htm :
+if MISSING_XSLT
+	@echo "****************************************************"
+	@echo "WARNING:  xsltproc and/or w3m were not found on your"
+	@echo "          system but $@ is out of date and needs"
+	@echo "          to be rebuilt.  Changes to $<"
+	@echo "          will be ignored."
+	@echo "****************************************************"
+else
 	${XSLTPROC} --stringparam program "${TARGETPROGRAM}" $(top_srcdir)/sci-wcalc/htmlpage.xsl $< > $@
+endif
 
 .htm.txt :
+if MISSING_XSLT
+	@echo "****************************************************"
+	@echo "WARNING:  xsltproc and/or w3m were not found on your"
+	@echo "          system but $@ is out of date and needs"
+	@echo "          to be rebuilt.  Changes to $<"
+	@echo "          will be ignored."
+	@echo "****************************************************"
+else
 	${W3M} -dump $< > $@
+endif
+
+##
+## Used by octave-wcalc and mex-wcalc
+##  TARGETPROGRAM will be set by the top level Makefile.am in one of those
+##  subdirectories
+
+.txt.m : 
+	@echo "****************************************************"
+	@echo "Generating ${TARGETPROGRAM} help file: $*.m"
+	@echo "****************************************************"
+	${AWK} 'BEGIN{s=0;} \
+		s==0{ \
+			printf("%%%-16s ",toupper($$1)); \
+			for(i=2;i<=NF;i++){printf("%s ",$$i);} \
+			printf("\n"); \
+			s=1; \
+			next; \
+		} \
+		{print "%  " $$0} \
+		END{printf("\n\n");}' $< > $@
+	cat $(srcdir)/copyright.m >> $@
 
