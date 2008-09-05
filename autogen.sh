ee17cbd7a8f7cbffbcb1639570bdf70c62e141e8
Dan McMahill <mcmahill@alum.mit.edu>
2008-09-05 07:41:37 -0400

    Remove several generated and/or automatically copied files from cvs.  They never really belonged here.
    Update autogen.sh to run libtoolize and also check versions of lots of the
    tools.

--- autogen.sh
+++ autogen.sh
@@ -1,31 +1,94 @@
-#! /bin/sh
+#!/bin/sh
 #
-# $Id: autogen.sh,v 1.3 2005/10/12 16:29:30 dan Exp $
-#
-# Run the various GNU autotools to bootstrap the build
-# system.  Should only need to be done once.
 
-# for now avoid using bash as not everyone has that installed
-CONFIG_SHELL=/bin/sh
-export CONFIG_SHELL
+# a leftover cache from a different version will cause no end of headaches
+rm -fr autom4te.cache
+
+############################################################################
+#
+# libtoolize (libtool)
+#
+
+echo "Checking libtoolize version..."
+libtoolize --version 2>&1 > /dev/null
+rc=$?
+if test $rc -ne 0 ; then
+    echo "Could not determine the version of libtool on your machine"
+    echo "libtool --version produced:"
+    libtool --version
+    exit 1
+fi
+lt_ver=`libtoolize --version | awk '{print $NF; exit}'`
+lt_maj=`echo $lt_ver | sed 's;\..*;;g'`
+lt_min=`echo $lt_ver | sed -e 's;^[0-9]*\.;;g'  -e 's;\..*$;;g'`
+lt_teeny=`echo $lt_ver | sed -e 's;^[0-9]*\.[0-9]*\.;;g'`
+echo "    $lt_ver"
+
+case $lt_maj in
+    0)
+        echo "You must have libtool >= 1.4.0 but you seem to have $lt_ver"
+        exit 1
+	;;
+
+    1)
+        if test $lt_min -lt 4 ; then
+            echo "You must have libtool >= 1.4.0 but you seem to have $lt_ver"
+            exit 1
+        fi
+        ;;
+
+    *)
+        echo "You are running a newer libtool than gerbv has been tested with."
+	echo "It will probably work, but this is a warning that it may not."
+	;;
+esac
+echo "Running libtoolize..."
+libtoolize --force --copy --automake || exit 1
+
+############################################################################
+#
+# aclocal
+
+echo "Checking aclocal version..."
+acl_ver=`aclocal --version | awk '{print $NF; exit}'`
+echo "    $acl_ver"
 
 echo "Running aclocal..."
-aclocal -I m4 $ACLOCAL_FLAGS || exit 1
-echo "Done with aclocal"
+aclocal $ACLOCAL_FLAGS || exit 1
+echo "... done with aclocal."
+
+############################################################################
+#
+# autoheader
+
+echo "Checking autoheader version..."
+ah_ver=`autoheader --version | awk '{print $NF; exit}'`
+echo "    $ah_ver"
 
 echo "Running autoheader..."
 autoheader || exit 1
-echo "Done with autoheader"
+echo "... done with autoheader."
+
+############################################################################
+#
+# automake
+
+echo "Checking automake version..."
+am_ver=`automake --version | awk '{print $NF; exit}'`
+echo "    $am_ver"
 
 echo "Running automake..."
-automake -a -c --foreign || exit 1
-echo "Done with automake"
+automake --foreign --force --copy --add-missing || exit 1
+echo "... done with automake."
+
+############################################################################
+#
+# autoconf
+
+echo "Checking autoconf version..."
+ac_ver=`autoconf --version | awk '{print $NF; exit}'`
+echo "    $ac_ver"
 
 echo "Running autoconf..."
 autoconf || exit 1
-echo "Done with autoconf"
-
-echo "Configure will need to be run manually"
-
-echo "All done with autogen.sh"
-
+echo "... done with autoconf."
